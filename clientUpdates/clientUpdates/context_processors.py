3e3504cc00e3b556500ddaf2bb04f343d7cd76c2
aksinghal86 <aksinghal.aks@gmail.com>
2024-11-25 09:26:40 -0800

    updated info bar with bootstrap grid styling

--- clientUpdates/clientUpdates/context_processors.py
+++ clientUpdates/clientUpdates/context_processors.py
@@ -1,16 +1,19 @@
 from clientUpdates.models import Pws, ClaimPws
+import logging
+
+logger = logging.getLogger('clientUpdates')
 
 def info_bar_context(request):
     if request.user.is_authenticated:
         try:
             pws_record = Pws.objects.get(form_userid=request.user.username)
             claim_pws_record = ClaimPws.objects.get(pwsid=pws_record.pwsid)
-            print('test. this is a test.')
+
             return {
                 'pws': pws_record,
                 'claim_pws': claim_pws_record,
             }
         except (Pws.DoesNotExist, ClaimPws.DoesNotExist):
-            # Optionally, log the error or return default values
+            logger.warning("Pws or ClaimPws record does not exist.")
             return {}
     return {}
