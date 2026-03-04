7387360accc99a7243abc020a2802a7b96e3f8f3
jfontaine-ehe <jfontaine@eheinc.com>
2026-03-04 10:13:52 -0500

    Created javascript folder and file. finished ability to delete forms. Adding text to landing page.

---
+++ clientUpdates/clientUpdates/static/js/main.js
@@ -0,0 +1,18 @@
+
+// ---------- phase2_landing_page -------------------
+
+document.getElementById('pwsFormDelete').addEventListener("submit", function(elem) {
+    console.log("in delete button");
+    if (!confirm("Are you sure you'd like to delete this form?")) {
+        elem.preventDefault();
+    }
+});
+
+document.getElementById('sourceFormDelete').addEventListener("submit", function(elem) {
+    if (!confirm("Are you sure you'd like to delete this form?")) {
+        elem.preventDefault();
+    }
+});
+
+console.log(document.getElementById('pwsFormDeleteButton'))
+
