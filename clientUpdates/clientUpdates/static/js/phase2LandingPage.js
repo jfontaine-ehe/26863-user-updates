dd49e5b7266659b9c6e4f6c5a2bf7d28e8215131
jfontaine <jfontaine@eheinc.com>
2026-03-18 14:04:35 -0400

    adding loader to add-form buttons on phase2 landing page

--- clientUpdates/clientUpdates/static/js/phase2LandingPage.js
+++ clientUpdates/clientUpdates/static/js/phase2LandingPage.js
@@ -1,9 +1,15 @@
 
 // ---------- phase2_landing_page -------------------
 
-let deleteButtons = Array.from(document.querySelectorAll('form'))
+const deleteButtons = Array.from(document.querySelectorAll('form'))
     .filter(form => form.querySelector('button.btn.btn-danger'));
-console.log(deleteButtons)
+const addPWSForm = document.getElementById('addPWSForm');
+const addSourceForm = document.getElementById('addSourceForm')
+const loaderContainer = document.getElementById('loader-container');
+const loader = document.getElementById('loader');
+
+const addFormButtons = Array(addPWSForm, addSourceForm)
+console.log(addFormButtons)
 
 deleteButtons.forEach(node =>node.addEventListener("click", function (elem) {
         if (!confirm("Are you sure you'd like to delete this form?")) {
@@ -11,3 +17,14 @@ deleteButtons.forEach(node =>node.addEventListener("click", function (elem) {
         }
     })
 );
+
+addFormButtons.forEach(el => el.addEventListener("click", function (el){
+
+    console.log("true")
+    el.disabled = true;
+    loaderContainer.style.display = 'flex';
+    loader.style.display = 'block';
+
+}))
+
+
